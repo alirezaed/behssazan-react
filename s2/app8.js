a8239adfd12b9a5983933ad367cb25c38ff7c55a
ali edalat <aliedalat.pr@gmail.com>
2021-04-18 09:58:16 +0430

    add babel

--- s2/app8.js
+++ s2/app8.js
@@ -3,5 +3,12 @@ const rootNode = document.getElementById('root');
 const node1  = document.createElement('span');
 node1.style = 'color:red';
 node1.innerText = "Hi Im Node"
+node1.onclick = function name(params) {
+    
+};
+
+node1.addEventListener('click',function(){
+
+});
 
 rootNode.appendChild(node1);
