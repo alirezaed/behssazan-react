2c682fd5d1e9503d0dd04d9a3774aa38e22a2183
ali edalat <aliedalat.pr@gmail.com>
2021-04-18 12:07:30 +0430

    after session 3

---
+++ s3/app.js
@@ -0,0 +1,3 @@
+const firstName = 'alireza';
+
+console.log(firstName);
---
+++ s3/firstProject/src/index.js
@@ -0,0 +1,13 @@
+const person1 ={
+    fname:'ali'
+}
+
+const person2 = {
+    lname:'re s za ei'
+    
+}
+
+const p = {
+    ...person1,
+    ...person2
+}
